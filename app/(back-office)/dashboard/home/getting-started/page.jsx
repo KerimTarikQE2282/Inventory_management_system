54c9f8c9d44e3001a61eb230db39045582606114
KerimTarikQE2282 <git config --global user.email you@example.com>
2024-07-07 23:04:15 +0300

    commit 2 nav bar almost done

--- app/(back-office)/dashboard/home/getting-started/page.jsx
+++ app/(back-office)/dashboard/home/getting-started/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function page() {
+function GettingStarted() {
   return (
     <div>
       <h1>Getting started</h1>
@@ -8,4 +8,4 @@ function page() {
   )
 }
 
-export default page
+export default GettingStarted
